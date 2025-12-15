d5232ec229b3315458b6b7a827570c5b98b42f12
StelleRainn R.M <3298257615@qq.com>
2025-12-15 16:38:28 +0800

    Initial Commit

---
+++ VoiceControlApp/VoiceControlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceControlAppApp.swift
+//  VoiceControlApp
+//
+//  Created by 徐友友 on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct VoiceControlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
